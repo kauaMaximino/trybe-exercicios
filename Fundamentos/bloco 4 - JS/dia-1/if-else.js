7d834149290ae1b0fbc12859f7fff343e27925e1
Kauã Maximino <kaua_maximino@outlook.com>
2021-11-26 16:07:21 -0300

    ex 3 de JS

---
+++ Fundamentos/bloco 4 - JS/dia-1/if-else.js
@@ -0,0 +1,11 @@
+const nota1 = 99;
+
+if (nota1 < 60) {
+    console.log ("Você foi reprovado")
+}
+else if (nota1 <= 80) {
+    console.log ("Você está na nossa lista de espera")
+}
+else {
+    console.log ("Você foi aprovado. Parabéns!")
+}

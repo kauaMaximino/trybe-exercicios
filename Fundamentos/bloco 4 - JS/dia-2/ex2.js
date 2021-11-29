820281bf97fe4db849c1a2ee1bd795b79e6c6a5c
kauamaximino <kaua_maximino@outlook.com>
2021-11-29 18:16:48 -0300

    ex 2 finalizado

--- Fundamentos/bloco 4 - JS/dia-2/ex2.js
+++ Fundamentos/bloco 4 - JS/dia-2/ex2.js
@@ -1,5 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
 
-for (let i = 0; i < numbers.length; i +=1) {
-    console.log(numbers[i]);
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] ) {
+   total = total + numbers[i];
+  }
 }
+
+console.log(total);

11bce1b09d4e8f7056b70faf4a7f621b8fc92950
Kauã Maximino <kaua_maximino@outlook.com>
2021-12-01 19:10:20 -0300

    exercicio triangulo

---
+++ Fundamentos/bloco 4 - JS/dia-1/triangulo.js
@@ -0,0 +1,9 @@
+let value1 = 60;
+let value2 = 60;
+let value3 = 60;
+
+if (value1 + value2 + value3 === 180) {
+    console.log("é um triangulo")
+} else {
+    console.log("Não é um triangulo")
+} 

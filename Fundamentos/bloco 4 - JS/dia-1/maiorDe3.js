2505aaffd57542d232987613fed6f140655af1f3
Kauã Maximino <kaua_maximino@outlook.com>
2021-12-01 18:24:36 -0300

    exercicio maior de 3

---
+++ Fundamentos/bloco 4 - JS/dia-1/maiorDe3.js
@@ -0,0 +1,11 @@
+let value1 = 3;
+let value2 = 3;
+let value3 = 21;
+
+if (value1 > value2 && value1 > value3) {
+    console.log (value1)
+} else if (value2 > value3 && value2 > value1) {
+    console.log (value2)
+} else {
+    console.log (value3)
+}

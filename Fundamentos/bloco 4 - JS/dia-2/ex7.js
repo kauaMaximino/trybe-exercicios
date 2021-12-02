d11ac31a6c73de23bfe2799ea535d82ebdc098fa
Kauã Maximino <kaua_maximino@outlook.com>
2021-12-02 12:18:37 -0300

    exercicio 8 finalizado

---
+++ Fundamentos/bloco 4 - JS/dia-2/ex7.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let smaller = Math.min(numbers)
+
+console.log(smaller)
+// let smaller;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] > 0 &&  numbers[i]< smaller) {
+//     smaller = numbers[i];
+//   }
+// }
+
+// console.log(smaller);

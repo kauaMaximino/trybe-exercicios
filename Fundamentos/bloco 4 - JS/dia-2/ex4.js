66f8ccc3bf826307d4877d20734b92777efb0d25
Kauã Maximino <kaua_maximino@outlook.com>
2021-12-02 11:06:02 -0300

    melhorando codigo do exercicio 4

--- Fundamentos/bloco 4 - JS/dia-2/ex4.js
+++ Fundamentos/bloco 4 - JS/dia-2/ex4.js
@@ -1,18 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let total = 0;
-valorDividido = 0
 
 for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] ) {
-   total = total + numbers[i]
-  }
+  total = total + numbers[i];
 }
 
-valorDividido = total / numbers.length
+valorDividido = total / numbers.length;
 
-if(valorDividido > 20) {
-    console.log("Valor maior que 20")
+if (valorDividido > 20) {
+  console.log("Valor maior que 20");
 } else {
-    console.log("Valor menor que 20")
+  console.log("Valor menor que 20");
 }
-

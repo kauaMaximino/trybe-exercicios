b2b80c82fd99580c8b9c20205a84bb1a61548607
Kauã Maximino <kaua_maximino@outlook.com>
2021-12-02 10:57:17 -0300

    melhorando código do exercicio 3

--- Fundamentos/bloco 4 - JS/dia-2/ex3.js
+++ Fundamentos/bloco 4 - JS/dia-2/ex3.js
@@ -1,11 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let total = 0;
-numsTotais = 0
 
 for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] ) {
-   total = total + numbers[i]
-  }
+  total = total + numbers[i];
 }
 
 console.log(total / numbers.length);

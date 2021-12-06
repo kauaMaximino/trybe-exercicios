874675dfa46b5ee42a1aaa01b8d1204353fa9e47
Kauã Maximino <kaua_maximino@outlook.com>
2021-12-06 18:55:37 -0300

    exercicio 2 ok

--- Fundamentos/bloco-5-DOM/dia-2/script.js
+++ Fundamentos/bloco-5-DOM/dia-2/script.js
@@ -2,3 +2,7 @@ let body = document.querySelector('#body');
 
 let h1 = document.createElement('h1');
 body.appendChild(h1);
+
+let main = document.createElement('main')
+body.appendChild(main);
+main.className = 'main-content'

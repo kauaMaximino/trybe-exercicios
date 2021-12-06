e46c8495e70221275c2a6cbd0019103e96fcd91f
Kauã Maximino <kaua_maximino@outlook.com>
2021-12-06 18:47:28 -0300

    exercicio 1 ok

---
+++ Fundamentos/bloco-5-DOM/dia-2/script.js
@@ -0,0 +1,4 @@
+let body = document.querySelector('#body');
+
+let h1 = document.createElement('h1');
+body.appendChild(h1);
--- Fundamentos/bloco-5-DOM/dia-2/teste-course2.js
+++ Fundamentos/bloco-5-DOM/dia-2/teste-course2.js
@@ -5,5 +5,10 @@ document.querySelector('#primeiroFilhoDoFilho').innerHTML = 'adicionando texto'
 
 document.querySelector('#pai').firstElementChild.innerText = 'outro texto'
 
-document.querySelector('#elementoOndeVoceEsta').nextSibling.style.color = 'red'
+// document.querySelector('#elementoOndeVoceEsta').nextSibling.style.color = 'red'
 
+let pai = document.querySelector('#pai')
+
+let aondeVcEsta2 = document.createElement('p')
+
+pai.appendChild(aondeVcEsta2)

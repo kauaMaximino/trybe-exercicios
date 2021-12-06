d904c23d20cb696bc9c6e1b039e503d6c5da022e
Kauã Maximino <kaua_maximino@outlook.com>
2021-12-06 15:26:10 -0300

    teste do course

---
+++ Fundamentos/bloco-5-DOM/dia-2/teste-course2.js
@@ -0,0 +1,9 @@
+// let ondeEsta = document.querySelector('#elementoOndeVoceEsta')
+document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = 'green'
+
+document.querySelector('#primeiroFilhoDoFilho').innerHTML = 'adicionando texto'
+
+document.querySelector('#pai').firstElementChild.innerText = 'outro texto'
+
+document.querySelector('#elementoOndeVoceEsta').nextSibling.style.color = 'red'
+

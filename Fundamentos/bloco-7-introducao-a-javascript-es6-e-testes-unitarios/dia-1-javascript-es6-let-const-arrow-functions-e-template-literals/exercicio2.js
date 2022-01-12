faf7b1babfdc00e9ad27e1a1e26bce657a5aa75a
Kaua Maximino <kaua_maximino@outlook.com>
2022-01-12 19:04:42 -0300

    ordenando array em ordem numerica usando o sort

---
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio2.js
@@ -0,0 +1,6 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+const ordener = (a, b) => a - b
+
+console.log(oddsAndEvens.sort(ordener)); // será necessário alterar essa linha 😉
--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercio1.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercio1.js
@@ -13,16 +13,14 @@
 //   testingScope(true);
 
 const testingScope = (escopo) => {
-  if (escopo) {
     let ifScope = "Não devo ser utilizada fora do meu escopo (if)";
+    const elseScope = "Não devo ser utilizada fora meu escopo (else)";
+  if (escopo) {
     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
     console.log(ifScope);
   } else {
-    const elseScope = "Não devo ser utilizada fora meu escopo (else)";
     console.log(elseScope);
   }
-
-  // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
 };
 
-testingScope(true);
+testingScope(false);

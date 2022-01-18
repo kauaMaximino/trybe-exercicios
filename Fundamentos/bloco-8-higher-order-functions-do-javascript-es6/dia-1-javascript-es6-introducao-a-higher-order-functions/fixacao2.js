fcad64876aaaec23c899c6285153633b57db83ff
Kaua Maximino <kaua_maximino@outlook.com>
2022-01-18 15:41:46 -0300

    desenvolvendo uma HOF (high order functions) e colocando algumas funcçoes como parametro

---
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fixacao2.js
@@ -0,0 +1,11 @@
+const acordando = () => 'Acordando!!';
+
+const coffe = () => 'Bora tomar café!!';
+
+const sleep = () => 'Partiu dormir!!';
+
+
+const doindThings = (func) => console.log(func());
+
+doindThings(sleep)
+

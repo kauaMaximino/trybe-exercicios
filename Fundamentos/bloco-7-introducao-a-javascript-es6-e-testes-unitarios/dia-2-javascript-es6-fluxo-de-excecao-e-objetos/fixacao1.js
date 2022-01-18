14c6a2b0e3eca95fd6bd6037cfa8aa6d92c7c9ab
Kaua Maximino <kaua_maximino@outlook.com>
2022-01-18 15:25:09 -0300

    criando arquivos do bloco 8

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/fixacao1.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/fixacao1.js
@@ -2,19 +2,30 @@
 
 //   console.log(sum(2, 3));
 
-const verifyIsNumber = (value1, value2) => {
-  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-    throw new Error('Os valores devem ser numéricos');
-  }
-};
+// const verifyIsNumber = (value1, value2) => {
+//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+//     throw new Error('Os valores devem ser numéricos');
+//   }
+// };
 
-const sum = (value1, value2) => {
-  try {
-    verifyIsNumber(value1, value2);
-    return value1 + value2;
-  } catch (error) {
-    throw error.message;
-  }
-};
+// const sum = (value1, value2) => {
+//   try {
+//     verifyIsNumber(value1, value2);
+//     return value1 + value2;
+//   } catch (error) {
+//     throw error.message;
+//   }
+// };
 
-console.log(sum(2, '3'));
+// console.log(sum(2, '3'));
+
+const string = 'isso é um taste, não me leve à sério';
+let counter = 0;
+const letter = 'a';
+for (index = 0; index < string.length; index += 1) {
+  if (string[index] === letter) {
+    counter += 1;
+  } 
+}
+
+console.log(counter)

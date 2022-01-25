7b03e2236edb2622a1f8b0790215793ee3d5457d
Kaua Maximino <kaua_maximino@outlook.com>
2022-01-25 09:51:02 -0300

    fixando map

---
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/map.js
@@ -0,0 +1,7 @@
+numbers = [1, 4, 67, 89, 99, 56, 34]
+
+const multipli = (numero, multiplicador) => numero * multiplicador;
+
+const multi = numbers.map((multipli ));
+
+console.log(multi.sort((a, b) => a-b));

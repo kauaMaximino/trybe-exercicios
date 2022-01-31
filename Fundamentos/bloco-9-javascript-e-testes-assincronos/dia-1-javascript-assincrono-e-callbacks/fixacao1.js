386edde3541248e5c29eddfc7d8076b5188b375e
Kaua Maximino <kaua_maximino@outlook.com>
2022-01-31 14:42:32 -0300

    arquivos do dia 2 do bloco 9

--- Fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/fixacao1.js
+++ Fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/fixacao1.js
@@ -1,8 +1,12 @@
-setTimeout(() => {
-  console.log('Comprar parafusos') // Comprar parafusos
-  console.log('Adicionar ao estoque') // Adicionar ao estoque
-}, 2000);
+// setTimeout(() => {
+//   console.log('Comprar parafusos') // Comprar parafusos
+//   console.log('Adicionar ao estoque') // Adicionar ao estoque
+// }, 2000);
 
-console.log('1 - Receber roda'); // 1 - Receber roda
-console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
-console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro
+// console.log('1 - Receber roda'); // 1 - Receber roda
+// console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
+// console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro
+
+const numbers = [1, 2, 3, 4]
+const result = numbers.reduce((accumulator, item) => accumulator + item, 0)
+console.log(result);

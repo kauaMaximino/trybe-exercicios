9984f59d331750eb25ad7cb88ff538295410424c
Kaua Maximino <kaua_maximino@outlook.com>
2022-02-01 19:34:05 -0300

    arquivos para exercicio do dia

--- Fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/fixacao1.js
+++ Fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/fixacao1.js
@@ -1,48 +1,65 @@
-const jsonInfo = `{
-  "muitasEmpresasUsam": [
-    "Google",
-    "Twitter",
-    "Facebook",
-    "etc..."
-  ],
-  "temVariasVantagens": [
-    "Legível",
-    "Fácil de usar",
-    "Leve",
-    "Popular",
-    "Versátil"
-  ],
-  "muitasLinguagensDaoSuporte": [
-    "Python",
-    "C",
-    "C++",
-    "Java",
-    "PHP"
-  ]
-}`;
+// const jsonInfo = `{
+//   "muitasEmpresasUsam": [
+//     "Google",
+//     "Twitter",
+//     "Facebook",
+//     "etc..."
+//   ],
+//   "temVariasVantagens": [
+//     "Legível",
+//     "Fácil de usar",
+//     "Leve",
+//     "Popular",
+//     "Versátil"
+//   ],
+//   "muitasLinguagensDaoSuporte": [
+//     "Python",
+//     "C",
+//     "C++",
+//     "Java",
+//     "PHP"
+//   ]
+// }`;
 
-const usoJSONPorque = JSON.parse(jsonInfo);
+// const usoJSONPorque = JSON.parse(jsonInfo);
 
-const corporationsList = document.getElementById('corporations-used-by');
-const advantagesList = document.getElementById('advantages');
-const languagesList = document.getElementById('languages-used-by');
+// const corporationsList = document.getElementById('corporations-used-by');
+// const advantagesList = document.getElementById('advantages');
+// const languagesList = document.getElementById('languages-used-by');
 
-usoJSONPorque.muitasEmpresasUsam.map((empresa) => {
-  const newLi = document.createElement('li');
-  newLi.innerText = empresa;
-  corporationsList.appendChild(newLi);
-});
+// usoJSONPorque.muitasEmpresasUsam.map((empresa) => {
+//   const newLi = document.createElement('li');
+//   newLi.innerText = empresa;
+//   corporationsList.appendChild(newLi);
+// });
 
-usoJSONPorque.temVariasVantagens.map((vantagens) => {
-  const newLi = document.createElement('li');
-  newLi.innerText = vantagens;
-  advantagesList.appendChild(newLi);
-});
+// usoJSONPorque.temVariasVantagens.map((vantagens) => {
+//   const newLi = document.createElement('li');
+//   newLi.innerText = vantagens;
+//   advantagesList.appendChild(newLi);
+// });
 
-usoJSONPorque.muitasLinguagensDaoSuporte.map((linguagens) => {
-  const newLi = document.createElement('li');
-  newLi.innerText = linguagens;
-  languagesList.appendChild(newLi);
-});
+// usoJSONPorque.muitasLinguagensDaoSuporte.map((linguagens) => {
+//   const newLi = document.createElement('li');
+//   newLi.innerText = linguagens;
+//   languagesList.appendChild(newLi);
+// });
 
 // wget 'https://pokeapi.co/api/v2/pokemon/ditto' -O - -q
+
+const ages = [
+  {age: 18},
+  {age: 19},
+  {age: 15},
+  {age: 15},
+  {age: 19},
+  {age: 18},
+  {age: 18},
+]
+
+const ageFrequency = ages.reduce((accumulator, { age }) => {
+  accumulator[age] = accumulator[age] + 1 || 1
+  return accumulator;
+}, {})
+
+console.log(ageFrequency);

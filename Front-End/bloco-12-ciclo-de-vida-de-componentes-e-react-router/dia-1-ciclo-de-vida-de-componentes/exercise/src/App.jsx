cc3503d57f903b6426cdb3c86f5abb290c385391
Kauã Maximino <kaua_maximino@outlook.com>
2022-03-16 16:17:49 -0300

    commitando tudo

--- Front-End/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercise/src/App.jsx
+++ Front-End/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercise/src/App.jsx
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
-import logo from './assets/trybemestar.png';
-import Timer from './components/Timer';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./assets/trybemestar.png";
+import "./App.css";
+import Timer from "./components/Timer";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
-      showTimer: false,
+      showTimer: false
     };
+
+    this.toggleTimer = this.toggleTimer.bind(this);
   }
 
-  toggleTimer = () => {
-    this.setState((prev) => ({ showTimer: !prev.showTimer }));
-  };
+  toggleTimer() {
+    this.setState((prevState) => ({ showTimer: !prevState.showTimer }));
+  }
 
   render() {
     const { showTimer } = this.state;
@@ -23,11 +25,10 @@ class App extends Component {
         <header>
           <img src={logo} alt="logo" />
         </header>
-
         <main>
           {showTimer && <Timer />}
           <button type="button" onClick={this.toggleTimer}>
-            {showTimer ? 'Desligar Timer' : 'Ligar Timer'}
+            {showTimer ? "Desligar Timer" : "Ligar Timer"}
           </button>
         </main>
       </div>

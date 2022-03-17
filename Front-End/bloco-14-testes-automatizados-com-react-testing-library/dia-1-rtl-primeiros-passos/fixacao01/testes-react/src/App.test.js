0a83ea1382ef6983ab22daf1604e47598d72b0ad
Kauã Maximino <kaua_maximino@outlook.com>
2022-03-16 23:27:51 -0300

    verificando se o formulario se comporta como o esperado

--- Front-End/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/fixacao01/testes-react/src/App.test.js
+++ Front-End/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/fixacao01/testes-react/src/App.test.js
@@ -1,11 +1,25 @@
 // App.test.js
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('Verificando se existe o campo Email.', () => {
+
+// Adicione esse teste.
+test('Verificando se o botão e o campo email estão funcionando.', () => {
   render(<App />);
+
+  const EMAIL_USER = 'email@email.com';
+
+  const textEmail = screen.getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor:');
+
+  const btnSend = screen.getByTestId('id-send');
   const inputEmail = screen.getByLabelText('Email');
-  expect(inputEmail).toBeInTheDocument();
-  expect(inputEmail).toHaveProperty('type', 'email');
+  userEvent.type(inputEmail, EMAIL_USER);
+  userEvent.click(btnSend);
+
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Valor: ${ EMAIL_USER }`);
 });

013983d5b9ed8236c77312b4c89201094878524b
Kauã Maximino <kaua_maximino@outlook.com>
2022-03-24 13:14:56 -0300

    criando pasta do bloco 15

--- Front-End/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicios-do-dia/service.js
+++ Front-End/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicios-do-dia/service.js
@@ -1,3 +1,4 @@
 const randomNumber = () => Math.floor(Math.random()*101);
+const numberRandomDiv = () => Math.floor(Math.random()*101);
 
-module.exports = {randomNumber}
+module.exports = {randomNumber, numberRandomDiv}
--- Front-End/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicios-do-dia/service.test.js
+++ Front-End/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicios-do-dia/service.test.js
@@ -1,5 +1,33 @@
-const service = require('./service')
+const service = require('./service');
 
 describe('exercicio 1', () => {
+  service.randomNumber = jest.fn().mockReturnValue(10);
 
+  it('Utilize o mock e defina o retorno padrão como 10.', () => {
+    expect(service.randomNumber()).toBe(10);
+  })
+
+  it('Teste se a função foi chamada', () => {
+    expect(service.randomNumber).toHaveBeenCalled();
+  })
+
+  it('Qual o retorno da função e quantas vezes foi chamada', () => {
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  })
+});
+
+describe('exercicio 2', () => {
+  service.numberRandomDiv = jest.fn().mockImplementationOnce((a, b) => a / b);
+
+  it('Defina o mock da função para a nova funcionalidade da função.', () => {
+    expect(service.numberRandomDiv(10, 2)).toBe(5)
+  })
+
+  it('Teste se a função foi chamada e a nova implementação de divisão foi aplicada.', () => {
+    expect(service.numberRandomDiv).toHaveBeenCalled()
+  })
+
+  it('Verifique se a aplicação da nova implementação acontece apenas uma vez.', () => {
+    expect(service.numberRandomDiv).toHaveBeenCalledTimes(1)
+  })
 })

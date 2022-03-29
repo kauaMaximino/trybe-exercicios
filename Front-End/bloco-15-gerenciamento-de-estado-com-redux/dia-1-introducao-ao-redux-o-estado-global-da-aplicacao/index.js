5fc48e7d4155443c9b60663ee04629ab954062c9
Kauã Maximino <kaua_maximino@outlook.com>
2022-03-29 13:45:13 -0300

    criando arquivos para o dia

---
+++ Front-End/bloco-15-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux-o-estado-global-da-aplicacao/index.js
@@ -0,0 +1,13 @@
+const Redux = require('redux');
+
+const ESTADO_INICIAL = {
+login: false,
+email: "",
+};
+
+const reducer = (state = ESTADO_INICIAL) => {
+return state;
+};
+
+const store = Redux.createStore(reducer);
+console.log(store.getState());

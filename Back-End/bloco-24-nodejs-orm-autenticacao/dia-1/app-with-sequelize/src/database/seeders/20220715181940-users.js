79a75a1c85dba61251cdf057e5456b8749a5569f
Kauã Maximino <kaua_maximino@outlook.com>
2022-07-15 15:41:07 -0300

    refactor: arrumando a arquitetura da aplicação

---
+++ Back-End/bloco-24-nodejs-orm-autenticacao/dia-1/app-with-sequelize/src/database/seeders/20220715181940-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const sequelize = require('sequelize');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('users', 
+    [
+      {
+        email: "teste@teste.com",
+        password_hash: "123456",
+        name: "Teste",
+        phone: "123456789",
+        created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        email: "teste@teste.com",
+        password_hash: "123456",
+        name: "Teste",
+        phone: "123456789",
+        created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    ]
+  ),
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};

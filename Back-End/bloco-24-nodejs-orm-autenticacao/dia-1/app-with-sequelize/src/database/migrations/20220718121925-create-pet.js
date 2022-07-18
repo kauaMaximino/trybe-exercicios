2699d3c2d8e47c514be06f040b45bccc85918f50
Kauã Maximino <kaua_maximino@outlook.com>
2022-07-18 09:23:41 -0300

    feat: criando migration de pet e configurando o model

---
+++ Back-End/bloco-24-nodejs-orm-autenticacao/dia-1/app-with-sequelize/src/database/migrations/20220718121925-create-pet.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Pets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Pets');
+  }
+};

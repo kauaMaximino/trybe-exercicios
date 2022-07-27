1533933660efbd8d78cfe296d06d8ec0eb02d5a9
Kauã Maximino <kaua_maximino@outlook.com>
2022-07-27 11:33:07 -0300

    entendendo sobre Classesmdo TS

---
+++ Back-End/bloco-26-typescript/dia-2/hero.ts
@@ -0,0 +1,24 @@
+interface Hero {
+  _name: string;
+  _power: string;
+  inspirador(frase: string): string;
+}
+
+class Hero {
+  _name: string;
+  _power: string;
+
+  constructor(name: string, power: string) {
+    this._name = name;
+    this._power = power;
+  }
+
+  inspirador(frase: string) {
+    return `${this._name}, com o poder ${this._power}, diz: ${frase}`;
+  }
+}
+
+const Hero1 = new Hero('Batman', 'Dinheiro');
+const Hero2 = new Hero('Superman', 'Laser');
+
+console.log(Hero2.inspirador('Você é o melhor, até sem dinheiro'));

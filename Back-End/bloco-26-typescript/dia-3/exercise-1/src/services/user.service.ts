b7ad993b3d43e557e8541669ee7a872ffef8baa6
Kauã Maximino <kaua_maximino@outlook.com>
2022-07-28 22:14:04 -0300

    um pouco mais de TS

---
+++ Back-End/bloco-26-typescript/dia-3/exercise-1/src/services/user.service.ts
@@ -0,0 +1,7 @@
+import { User } from '../interfaces/user.interface';
+import * as userModel from '../models/user.models';
+
+export async function getAll() {
+  const data = await userModel.getAll();
+  return data;
+}

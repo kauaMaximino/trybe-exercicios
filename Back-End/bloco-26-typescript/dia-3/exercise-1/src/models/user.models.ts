7897ed60faa72dbedc9cc1a36ff2611aeaddbc06
Kauã Maximino <kaua_maximino@outlook.com>
2022-07-28 22:05:42 -0300

    feat: user models

--- Back-End/bloco-26-typescript/dia-3/exercise-1/src/models/user.models.ts
+++ Back-End/bloco-26-typescript/dia-3/exercise-1/src/models/user.models.ts
@@ -4,6 +4,5 @@ import { ResultSetHeader } from 'mysql2';
 
 export async function getAll(): Promise<User[]> {
   const [rows] = await connection.execute('SELECT * FROM Users');
-  console.log(rows);
   return rows as User[];
 }

f52e6e733839948c632cb73444ca2e5cec8fac3e
Kauã Maximino <kaua_maximino@outlook.com>
2022-08-09 10:11:52 -0300

    entendo sobre o constructor

--- Back-End/bloco-27-poo-solid/poo-typescript/Personagem.ts
+++ Back-End/bloco-27-poo-solid/poo-typescript/Personagem.ts
@@ -1,1 +1,22 @@
-// energia, vida, ataque, defesa e nome
+//nome, energia, vida, ataque e defesa
+
+class Personagem {
+
+  nome: string;
+  energia: number;
+  vida: number;
+  ataque: number;
+  defesa: number;
+
+  constructor(nome: string, energia: number, vida: number, ataque: number, defesa: number) {
+    this.nome = nome;
+    this.energia = energia;
+    this.vida = vida;
+    this.ataque = ataque;
+    this.defesa = defesa;
+  }
+}
+
+const person: Personagem = new Personagem('Personagem', 100, 100, 10, 10);
+
+console.log(person.defesa);
